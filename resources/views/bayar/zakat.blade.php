61f1e7b76590d8c2b7bcd716d0d4dab9cd191131
Ilham Setiaji <ilham.ahmadz18@gmail.com>
2022-08-15 07:31:13 +0700

    Added mailing features

--- resources/views/bayar/zakat.blade.php
+++ resources/views/bayar/zakat.blade.php
@@ -3,15 +3,39 @@
 <!--Form-->
 <section class="page-section pb-0 p-0" id="about">
     <div class="container-fluid" style="background-image:url('assets/img/kraton-2.png'); padding-top:5%;">
+        @if ($errors->any())
+        @foreach ($errors->all() as $error)
+        <div class="alert alert-warning alert-dismissible show fade">
+            <div class="alert-body">
+                <button class="close" data-dismiss="alert">
+                    <span>&times;</span>
+                </button>
+                {{ $error }}
+            </div>
+        </div>
+        @endforeach
+        @endif
+        @if (session('status'))
+        <div class="alert alert-info alert-dismissible show fade">
+            <div class="alert-body">
+                <button class="close" data-dismiss="alert">
+                    <span>&times;</span>
+                </button>
+                {{ session('status') }}
+            </div>
+        </div>
+        @endif
         <div class="row row-zakat">
             <div class="col-md-5" id='hide-content'>
-                <div class="right-content mt-5" style="padding-left: 10%; padding-right: 10%; width: 100%; height: auto; position: relative; transform: translateY(8%);">
+                <div class="right-content mt-5"
+                    style="padding-left: 10%; padding-right: 10%; width: 100%; height: auto; position: relative; transform: translateY(8%);">
                     <div class="latin text-white" style="text-align: left; font-size: clamp(14px, 3vw, 18px);">
                         Selamat datang di platform ZIS online BAZNAS
                         <br>
                         Kota Surakarta
                     </div>
-                    <div class="latin text-white mt-5" style="text-align: left; font-size: clamp(20px, 3vw, 36px); font-weight: 700;">
+                    <div class="latin text-white mt-5"
+                        style="text-align: left; font-size: clamp(20px, 3vw, 36px); font-weight: 700;">
                         SIAP BAYAR ZAKAT <br>
                         SEKARANG?
                     </div>
@@ -20,7 +44,8 @@
                         bersama BAZNAS Surakarta melalui media <br>
                         online
                     </div>
-                    <div class="latin text-white" style="text-align: left; font-size: clamp(10px, 3vw, 15px); margin-top: 100px;">
+                    <div class="latin text-white"
+                        style="text-align: left; font-size: clamp(10px, 3vw, 15px); margin-top: 100px;">
                         Periode
                     </div>
                     <h3 class="text-warning mt-3"><strong>2021/2022</strong></h3>
@@ -40,7 +65,8 @@
                         @csrf
                         <div class="form-group mt-4">
                             <label for="select-zakat" class="form-label">Jenis Zakat</label>
-                            <select class="form-select form-control text-muted" aria-label="Default select example" id="select-zakat" name="jenis">
+                            <select class="form-select form-control text-muted" aria-label="Default select example"
+                                id="select-zakat" name="jenis">
                                 <option value="Fitrah">Fitrah</option>
                                 <option value="Maal">Maal</option>
                                 <option value="Fidyah">Fidyah</option>
@@ -50,7 +76,8 @@
                         </div>
                         <div class="form-group mt-4">
                             <label for="nominal-zakat" class="form-label">Nominal Zakat</label>
-                            <input type="number" min="1" placeholder="Masukan nominal min Rp 10.000" class="form-control text-muted" id="nominal-zakat" name="nominal">
+                            <input type="number" min="1" placeholder="Masukan nominal min Rp 10.000"
+                                class="form-control text-muted" id="nominal-zakat" name="nominal">
                         </div>
                         <div class="form-group mt-4">
                             <label for="nominal-zakat" class="form-label">Bukti Pembayaran</label>
@@ -63,15 +90,18 @@
                         </div>
                         <div class="form-group mt-4">
                             <label for="nama" class="form-label">Nama Lengkap</label>
-                            <input type="text" placeholder="Masukkan nama lengkap" class="form-control text-muted" id="nama" name="name">
+                            <input type="text" placeholder="Masukkan nama lengkap" class="form-control text-muted"
+                                id="nama" name="name">
                         </div>
                         <div class="form-group mt-4">
                             <label for="telp" class="form-label">Nomor Telefon</label>
-                            <input type="number" placeholder="Masukkan nomor WA" class="form-control text-muted" id="telp" name="phone">
+                            <input type="number" placeholder="Masukkan nomor WA" class="form-control text-muted"
+                                id="telp" name="phone">
                         </div>
                         <div class="form-group mt-4">
                             <label for="email" class="form-label">Email</label>
-                            <input type="email" placeholder="Masukkan alamat email" class="form-control text-muted" id="email" name="email">
+                            <input type="email" placeholder="Masukkan alamat email" class="form-control text-muted"
+                                id="email" name="email">
                         </div>
                         {{-- <div class="form-check mt-3">
                             <label class="form-check-label text-dark" for="flexCheckChecked">
@@ -110,25 +140,30 @@
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
-            <div class="modal-header justify-content-center" style="background-color: #D9D9D9; border: 1px; border-color: black; border-style: solid;">
+            <div class="modal-header justify-content-center"
+                style="background-color: #D9D9D9; border: 1px; border-color: black; border-style: solid;">
                 <h5 class="modal-title" id="exampleModalLabel">SCAN QRIS</h5>
             </div>
-            <div class="modal-body" href="#" data-bs-toggle="modal" data-bs-target="#exampleModal3" style="background-color: #fff; border: 1px; border-color: black; border-style: solid;">
+            <div class="modal-body" href="#" data-bs-toggle="modal" data-bs-target="#exampleModal3"
+                style="background-color: #fff; border: 1px; border-color: black; border-style: solid;">
                 QRIS Doku
                 &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
                 &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;&nbsp;&nbsp;
                 <img src="assets/img/portfolio/logo/iconqris.png" style="width: 25%;">
             </div>
-            <div class="modal-header justify-content-center" style="background-color: #D9D9D9; border: 1px; border-color: black; border-style: solid;">
+            <div class="modal-header justify-content-center"
+                style="background-color: #D9D9D9; border: 1px; border-color: black; border-style: solid;">
                 <h5 class="modal-title" id="exampleModalLabel">Transfer Bank</h5>
             </div>
-            <div class="modal-body justify-content-center" style="border: 1px; border-color: black; border-style: solid;">
+            <div class="modal-body justify-content-center"
+                style="border: 1px; border-color: black; border-style: solid;">
                 <a href="#" class="text-dark" data-bs-toggle="modal" data-bs-target="#exampleModal2">Bank Jateng</a>
                 &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
                 &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
                 <img src="assets/img/portfolio/logo/iconbankjateng.png" style="width: 25%;">
             </div>
-            <div class="modal-body justify-content-center" style="border: 1px; border-color: black; border-style: solid;">
+            <div class="modal-body justify-content-center"
+                style="border: 1px; border-color: black; border-style: solid;">
                 Bank BSI
                 &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
                 &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;
@@ -146,7 +181,8 @@
                     BAZIS Kota Surakarta</h5>
             </div>
             <div class="modal-body">
-                <div class="card col-sm-10" style="border-radius: 15px; position: absolute; transform: translate(7%, -65%);">
+                <div class="card col-sm-10"
+                    style="border-radius: 15px; position: absolute; transform: translate(7%, -65%);">
                     <div class="card-header" style="color: #fff;">
                         <div class="row">
                             <div class="col-3">
@@ -154,7 +190,8 @@
                             </div>
                             <div class="col-6"></div>
                             <div class="col-3">
-                                <h6 style="color: black; font-size: small;">Bayar dalam <b style="color: #1400FF;">23.45.50</b></h6>
+                                <h6 style="color: black; font-size: small;">Bayar dalam <b
+                                        style="color: #1400FF;">23.45.50</b></h6>
                             </div>
                         </div>
                     </div>
@@ -175,16 +212,20 @@
                             <div style="color: black; font-size: large;"><b>QRIS Doku</b></div>
                         </div>
                         <div class="col-2"></div>
-                        <div class="col-5"><img src="assets/img/portfolio/logo/iconqris.png" style="width: 80%; transform: translate(30%, -40%);"></b></h6>
+                        <div class="col-5"><img src="assets/img/portfolio/logo/iconqris.png"
+                                style="width: 80%; transform: translate(30%, -40%);"></b></h6>
                         </div>
                     </div>
                     <div class="col-12">
-                        <div style="color: black;"><img src="assets/img/qris.png" style="width: 50%; transform: translate(50%, 0%);"></div>
+                        <div style="color: black;"><img src="assets/img/qris.png"
+                                style="width: 50%; transform: translate(50%, 0%);"></div>
                     </div>
                 </div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-primary" style="background-color: #FF9900; border-color: #FF9900; transform: translate(-55%);" data-bs-toggle="modal" data-bs-target="#exampleModal4">SAYA SUDAH MEMBAYAR</button>
+                <button type="button" class="btn btn-primary"
+                    style="background-color: #FF9900; border-color: #FF9900; transform: translate(-55%);"
+                    data-bs-toggle="modal" data-bs-target="#exampleModal4">SAYA SUDAH MEMBAYAR</button>
             </div>
         </div>
     </div>
@@ -198,7 +239,8 @@
                     BAZIS Kota Surakarta</h5>
             </div>
             <div class="modal-body">
-                <div class="card col-sm-10" style="border-radius: 15px; position: absolute; transform: translate(7%, -65%);">
+                <div class="card col-sm-10"
+                    style="border-radius: 15px; position: absolute; transform: translate(7%, -65%);">
                     <div class="card-header" style="color: #fff;">
                         <div class="row">
                             <div class="col-3">
@@ -206,7 +248,8 @@
                             </div>
                             <div class="col-6"></div>
                             <div class="col-3">
-                                <h6 style="color: black; font-size: small;">Bayar dalam <b style="color: #1400FF;">23.45.50</b></h6>
+                                <h6 style="color: black; font-size: small;">Bayar dalam <b
+                                        style="color: #1400FF;">23.45.50</b></h6>
                             </div>
                         </div>
                     </div>
@@ -227,7 +270,8 @@
                             <div style="color: black; font-size: large;"><b>Bank Jateng</b></div>
                         </div>
                         <div class="col-2"></div>
-                        <div class="col-5"><img src="assets/img/portfolio/logo/iconbankjateng.png" style="width: 80%; transform: translate(30%, -40%);"></b></h6>
+                        <div class="col-5"><img src="assets/img/portfolio/logo/iconbankjateng.png"
+                                style="width: 80%; transform: translate(30%, -40%);"></b></h6>
                         </div>
                     </div>
                     <div class="col-12">
@@ -251,7 +295,9 @@
                 </div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-primary" style="background-color: #FF9900; border-color: #FF9900; transform: translate(-55%);" data-bs-toggle="modal" data-bs-target="#exampleModal4">SAYA SUDAH MEMBAYAR</button>
+                <button type="button" class="btn btn-primary"
+                    style="background-color: #FF9900; border-color: #FF9900; transform: translate(-55%);"
+                    data-bs-toggle="modal" data-bs-target="#exampleModal4">SAYA SUDAH MEMBAYAR</button>
             </div>
         </div>
     </div>
@@ -294,25 +340,30 @@
 <div class="modal fade" id="exampleModal1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
-            <div class="modal-header justify-content-center" style="background-color: #D9D9D9; border: 1px; border-color: black; border-style: solid;">
+            <div class="modal-header justify-content-center"
+                style="background-color: #D9D9D9; border: 1px; border-color: black; border-style: solid;">
                 <h5 class="modal-title" id="exampleModalLabel">SCAN QRIS</h5>
             </div>
-            <div class="modal-body" style="background-color: #fff; border: 1px; border-color: black; border-style: solid;">
+            <div class="modal-body"
+                style="background-color: #fff; border: 1px; border-color: black; border-style: solid;">
                 QRIS Doku
                 &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
                 &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;&nbsp;&nbsp;
                 <img src="assets/img/portfolio/logo/iconqris.png" style="width: 25%;">
             </div>
-            <div class="modal-header justify-content-center" style="background-color: #D9D9D9; border: 1px; border-color: black; border-style: solid;">
+            <div class="modal-header justify-content-center"
+                style="background-color: #D9D9D9; border: 1px; border-color: black; border-style: solid;">
                 <h5 class="modal-title" id="exampleModalLabel">Transfer Bank</h5>
             </div>
-            <div class="modal-body justify-content-center text-dark" style="border: 1px; border-color: black; border-style: solid;">
+            <div class="modal-body justify-content-center text-dark"
+                style="border: 1px; border-color: black; border-style: solid;">
                 Bank Jateng
                 &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
                 &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
                 <img src="assets/img/portfolio/logo/iconbankjateng.png" style="width: 25%;">
             </div>
-            <div class="modal-body justify-content-center" style="border: 1px; border-color: black; border-style: solid;">
+            <div class="modal-body justify-content-center"
+                style="border: 1px; border-color: black; border-style: solid;">
                 Bank BSI
                 &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
                 &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;
