80c8caf811c5b8c89cd6979126ac3567ca9afe2d
Ridho Fata Ulwan <84189579+ridhofataulwan@users.noreply.github.com>
2022-09-12 09:34:21 +0700

    Fix layanan rekening's form (add, edit)

--- resources/views/admin/layanan-rekening/edit.blade.php
+++ resources/views/admin/layanan-rekening/edit.blade.php
@@ -29,29 +29,35 @@
                                         <form action="{{ url('admin/layanan/rekening/'.$rek->id.'/edit') }}" method="POST" enctype="multipart/form-data">
                                             @csrf
                                             <div class="form-group row mb-4">
-                                                <label
-                                                    class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Nomor Rekening</label>
+                                                <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Nomor Rekening</label>
                                                 <div class="col-sm-12 col-md-7">
                                                     <input type="number" min="0" class="form-control" name="no_rek" value="{{ $rek->no_rek }}">
                                                 </div>
                                             </div>
-
                                             <div class="form-group row mb-4">
-                                                <label
-                                                    class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Gambar</label>
+                                                <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Jenis Rekening</label>
+                                                <div class="col-sm-12 col-md-7">
+                                                    <select type="select" min="0" class="form-control" name="jenis_rek">
+                                                        <option value="zakat">Zakat</option>
+                                                        <option value="infaq">Infaq</option>
+                                                        <option value="infaq">Sedekah</option>
+                                                        <option value="fidyah">Fidyah</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                            <div class="form-group row mb-4">
+                                                <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Gambar</label>
                                                 <div class="col-sm-12 col-md-7">
                                                     <input type="file" class="form-control" name="image">
-                                                    <img src="{{ asset($rek->image) }}" alt=""
-                                                        style="height: 200px; width:400px;" class="mt-4">
+                                                    <img src="{{ asset($rek->image) }}" alt="" style="height: 200px; width:400px;" class="mt-4">
                                                     @error('image')
                                                     <span class="text-danger"> {{ $message }} </span>
                                                     @enderror
                                                 </div>
                                             </div>
-    
+
                                             <div class="form-group row mb-4">
-                                                <label
-                                                    class="col-form-label text-md-right col-12 col-md-3 col-lg-3"></label>
+                                                <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3"></label>
                                                 <div class="col-sm-12 col-md-7">
                                                     <button type="submit" class="btn btn-primary">Tambah</button>
                                                 </div>
@@ -75,32 +81,31 @@
 
     <script type="text/javascript">
         var rupiah = document.getElementById('rupiah');
-                rupiah.addEventListener('keyup', function(e){
-                    // tambahkan 'Rp.' pada saat form di ketik
-                    // gunakan fungsi formatRupiah() untuk mengubah angka yang di ketik menjadi format angka
-                    rupiah.value = formatRupiah(this.value, 'Rp. ');
-                });
-         
-                /* Fungsi formatRupiah */
-                function formatRupiah(angka, prefix){
-                    var number_string = angka.replace(/[^,\d]/g, '').toString(),
-                    split   		= number_string.split(','),
-                    sisa     		= split[0].length % 3,
-                    rupiah     		= split[0].substr(0, sisa),
-                    ribuan     		= split[0].substr(sisa).match(/\d{3}/gi);
-         
-                    // tambahkan titik jika yang di input sudah menjadi angka ribuan
-                    if(ribuan){
-                        separator = sisa ? '.' : '';
-                        rupiah += separator + ribuan.join('.');
-                    }
-         
-                    rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
-                    return prefix == undefined ? rupiah : (rupiah ? 'Rp. ' + rupiah : '');
-                }
-        </script>
-        
+        rupiah.addEventListener('keyup', function(e) {
+            // tambahkan 'Rp.' pada saat form di ketik
+            // gunakan fungsi formatRupiah() untuk mengubah angka yang di ketik menjadi format angka
+            rupiah.value = formatRupiah(this.value, 'Rp. ');
+        });
+
+        /* Fungsi formatRupiah */
+        function formatRupiah(angka, prefix) {
+            var number_string = angka.replace(/[^,\d]/g, '').toString(),
+                split = number_string.split(','),
+                sisa = split[0].length % 3,
+                rupiah = split[0].substr(0, sisa),
+                ribuan = split[0].substr(sisa).match(/\d{3}/gi);
+
+            // tambahkan titik jika yang di input sudah menjadi angka ribuan
+            if (ribuan) {
+                separator = sisa ? '.' : '';
+                rupiah += separator + ribuan.join('.');
+            }
+
+            rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
+            return prefix == undefined ? rupiah : (rupiah ? 'Rp. ' + rupiah : '');
+        }
+    </script>
+
 </body>
 
 </html>
-
