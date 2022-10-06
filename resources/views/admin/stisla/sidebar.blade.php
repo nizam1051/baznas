c423bcb66e24b2b3345c78ddaebbdb0d25d9fb6c
Ridho Fata Ulwan <84189579+ridhofataulwan@users.noreply.github.com>
2022-10-06 10:34:31 +0700

    Add sidebar post component (Admin)

--- resources/views/admin/stisla/sidebar.blade.php
+++ resources/views/admin/stisla/sidebar.blade.php
@@ -63,8 +63,7 @@
 <div class="main-sidebar" tabindex="1" style="overflow: hidden; outline: none;">
     <aside id="sidebar-wrapper">
         <div class="sidebar-brand p-1 mb-5">
-            <a href="/" style="font-family: Arial; font-weight:bold; font-size: 1.7em"><img style="width: 3em;"
-                    src="{{url('/assets/img/portfolio/logo/logo3.png')}}"> BAZNAS</a>
+            <a href="/" style="font-family: Arial; font-weight:bold; font-size: 1.7em"><img style="width: 3em;" src="{{url('/assets/img/portfolio/logo/logo3.png')}}"> BAZNAS</a>
         </div>
         <div class="sidebar-brand sidebar-brand-sm">
             <img style="width: 50%;" src="{{url('/assets/img/portfolio/logo/logo3.png')}}">
@@ -92,18 +91,7 @@
             <li class="dropdown">
                 <a href="#" class="nav-link has-dropdown"><i class="fas fa-newspaper"></i> <span>Kabar</span></a>
                 <ul class="dropdown-menu">
-                    <li>
-                        <a class="nav-link" href="{{url('/admin/post/kabar-zakat')}}"> <span>Data Kabar Zakat</span></a>
-                    </li>
-                    <li>
-                        <a class="nav-link" href="{{url('/admin/post/artikel')}}"> <span>Data Artikel</span></a>
-                    </li>
-                    <li>
-                        <a class="nav-link" href="{{url('/admin/post/inspirasi')}}"> <span>Data Inspirasi</span></a>
-                    </li>
-                    <li>
-                        <a class="nav-link" href="{{url('/admin/galeri')}}"> <span>Data Galeri</span></a>
-                    </li>
+                    <x-admin.sidebar.post />
                 </ul>
             </li>
             <li class="dropdown">
@@ -118,6 +106,9 @@
                                 Pembayar</span></a>
                     </li>
                 </ul>
+            </li>
+            <li>
+                <a class="nav-link" href="{{url('/admin/galeri')}}"><i class="fas fa-image"></i> <span>Galeri</span></a>
             </li>
             <li>
                 <a class="nav-link" href="{{url('/admin/pesan')}}"><i class="fas fa-envelope"></i>
